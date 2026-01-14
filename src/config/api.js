25c006a9cf20b8a44befc06a0441d1794647a053
BrayanRNothing <eligiobrayanrod@gmail.com>
2026-01-14 10:01:36 -0600

    quite de local a prod

--- src/config/api.js
+++ src/config/api.js
@@ -2,8 +2,8 @@
 const isDevelopment = import.meta.env.MODE === 'development';
 
 // URL del Backend - Usar localhost para desarrollo
-const API_URL = 'http://localhost:4000';  // Backend local
+//const API_URL = 'http://localhost:4000';  // Backend local
 
 // URL del Backend - Railway (comentado)
-//const API_URL = 'https://updm-infiniguard-production.up.railway.app';
+const API_URL = 'https://updm-infiniguard-production.up.railway.app';
 export default API_URL;
