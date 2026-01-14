da4539b4c9073f3aecf3c572f4946a59b27a9ec6
BrayanRNothing <eligiobrayanrod@gmail.com>
2026-01-14 10:04:40 -0600

    nueva enlace railway

--- src/config/api.js
+++ src/config/api.js
@@ -3,7 +3,7 @@ const isDevelopment = import.meta.env.MODE === 'development';
 
 // URL del Backend - Usar localhost para desarrollo
 //const API_URL = 'http://localhost:4000';  // Backend local
-
+// focused-presence-production-6e28.up.railway.app
 // URL del Backend - Railway (comentado)
-const API_URL = 'https://updm-infiniguard-production.up.railway.app';
+const API_URL = 'https://focused-presence-production-6e28.up.railway.app';
 export default API_URL;
